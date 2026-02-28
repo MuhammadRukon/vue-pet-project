b564edf8f044454aefe080da51786600c3a8c888
muhammadrukon <muhammad.rukon242@gmail.com>
2026-03-01 01:00:43 +0600

    chore: vue + tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="w-full h-[calc(100vh-40px)]">
+    <Main />
+  </main>
+
+  <footer class="bg-black text-white text-center py-2">
+    <span>Being developed by Rukon. {{ new Date().getFullYear() }}</span>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import Main from './components/Main.vue'
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <select
+      v-model="selectedOption"
+      class="min-w-20 border border-gray-600 p-2 rounded-lg focus:outline-none"
+    >
+      <option :value="undefined" disabled>Select option</option>
+      <option v-for="item in options" :key="item.id" :value="item.id">
+        {{ item.name }}
+      </option>
+    </select>
+
+    <button>show value</button>
+  </main>
+</template>
+
+<script setup lang="ts">
+interface IOption {
+  id: number
+  name: string
+}
+import { ref, watch } from 'vue'
+const options = ref<IOption[]>([
+  { id: 1, name: 'one' },
+  { id: 2, name: 'two' },
+  { id: 3, name: 'three' },
+])
+
+const selectedOption = ref<IOption>()
+
+watch(selectedOption, (val) => {
+  console.log('value', val)
+})
+</script>
