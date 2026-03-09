a704628ea6a254443ab8229d0a49a45337d6539a
muhammadrukon <muhammad.rukon242@gmail.com>
2026-03-10 01:17:30 +0600

    chore: zod package update

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,18 +1,18 @@
-import z from 'zod'
+import { z } from 'zod'
 
 export const loginSchema = z.object({
-  email: z.email('Invalid email'),
-  password: z.string('Enter password'),
+  email: z.string().email('Invalid email'),
+  password: z.string().min(1, 'Enter password'),
 })
 
 export const registerSchema = z
   .object({
-    name: z.string('Enter name').min(2, 'Enter full name'),
-    email: z.email('Invalid email'),
-    confirmPassword: z.string('Enter password').min(4, 'Minimum 4 characters'),
-    password: z.string('Enter password').min(4, 'Minimum 4 characters'),
+    name: z.string().min(2, 'Enter full name'),
+    email: z.string().email('Invalid email'),
+    confirmPassword: z.string().min(4, 'Minimum 4 characters'),
+    password: z.string().min(4, 'Minimum 4 characters'),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    error: "Passwords don't match",
+    message: "Passwords don't match",
     path: ['confirmPassword'],
   })
